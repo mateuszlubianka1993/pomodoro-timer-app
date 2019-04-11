c01b5a4f3a7eb44e256c1ec8805ea9e864523478
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-04-11 13:31:59 +0200

    add semantic-ui and fonts awesome

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,15 @@ import React from 'react';
 const Header = () => {
 
     return (
-        <header>
-            <h1>Pomodoro Timer</h1>
+        <header className="header-container ui centered grid">
+            <div className="ui centered row header-row">
+                <div className="column">
+                    <i class="far fa-clock"></i>
+                </div>
+                <div className="six wide column">
+                    <h1>Pomodoro Timer</h1>
+                </div>
+            </div>
         </header>
     );
 };

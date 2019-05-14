8b1c3389ef61b2fc989b94aac0fc387927868a48
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-05-14 14:05:45 +0200

    Updates

---
+++ precache-manifest.ddc2f1b3c950b975b70254f4df8abd2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fc8ba6baaea144199c8",
+    "url": "/pomodoro-timer-app/static/js/runtime~main.6f33164c.js"
+  },
+  {
+    "revision": "95224fbbbcb23d945806",
+    "url": "/pomodoro-timer-app/static/js/main.58d250d6.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/pomodoro-timer-app/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "95224fbbbcb23d945806",
+    "url": "/pomodoro-timer-app/static/css/main.4915e45d.chunk.css"
+  },
+  {
+    "revision": "f7aaa890635d2dfb87a40e9c16145ba0",
+    "url": "/pomodoro-timer-app/index.html"
+  }
+];
